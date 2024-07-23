800b15e828ba4710949558dbae5a82df99050e32
PXXxBADBOYxXXP <marcojosepro2007@gmail.com>
2024-07-22 18:22:11 -0600

    docs(ServletHuespedes):agregue documentacion al servlet de entidad Huespedes

--- src/main/java/com/techtitans/servlet/HuespedesServlet.java
+++ src/main/java/com/techtitans/servlet/HuespedesServlet.java
@@ -19,29 +19,41 @@ import java.util.List;
  *
  * @author Marco Bolaños
  */
+//componente que se usa para desarrollo web de nombre "HuespedesServlet" su valor es igual a 
+// /Huespedes-servlet
 @WebServlet(name = "HuespedesServlet", value = {"/huespedes-servlet"})
+//propiedad Multipart config
 @MultipartConfig
+// Clase Huespedes Servlet que hereda HttpServlet
 public class HuespedesServlet extends HttpServlet{
-    
+    //Variable HuespedesService la cual defini como "huespedesService" 
     private HuespedesService huespedesService = new HuespedesService();
-    
+    //Propiedad Override
     @Override
+    // Metodo publico que no espera recibir parametro "init"
     public void init() throws ServletException{
       super.init();
       
       this.huespedesService =  new HuespedesService();
     }
     
-    
-    @Override 
+    //Propiedad Override
+    @Override
+    // Metodo protegido doGet el cual envia las peticiones
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        //arreglo de tipo lista llamado Huespedes difini variable metodos esto es igual a huespedesService
+        //y esto retorna la lista de Huespedes
         List<Huespedes> metodos = huespedesService.listaHuespedes();
+        //metodos recorre a base de for each este recorrido se almacena en la variable "p" y imprimo dicha variable
         metodos.forEach(p -> System.out.println(p));
+        //Arequest le seteamos Huespedes y la variable metodos
         request.setAttribute("Huespedes", metodos);
+        // Sirve para darle ombre a la direccion del listar de la entidad
         request.getRequestDispatcher("/Lista-Huespedes/listar-Huespedes.jsp").forward(request, response); 
     }
-    
+    //Metodo void agregarHuespedes recibe como prametros la request y response
     private void agregarHuespedes(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        //Request para obtener los atributos de la base de datos
         int idHuesped = 0;
         String nit = request.getParameter("nit");
         int Dpi = Integer.parseInt(request.getParameter("dpi"));
@@ -51,6 +63,7 @@ public class HuespedesServlet extends HttpServlet{
         String telefonoHuesped = request.getParameter("telefono");
         String password = request.getParameter("contraseña");
         Huespedes metodo = new Huespedes(idHuesped, nit, Dpi, nombreHuesped, apellidoHuesped, Email, telefonoHuesped, password);
+        //huespedesService mandamos a llamar al metodo agregarHuespedes y lo almacenamos en metodo
         huespedesService.agregarHuespedes(metodo); 
         
         response.sendRedirect(request.getContextPath() + "/huespedes-servlet");

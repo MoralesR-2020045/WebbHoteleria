b5d0ba64b7d982ad83e7cf9b7f5b1c036b11b926
MoralesR-2020045 <rmorales215327@gmail.com>
2024-07-25 17:03:49 -0600

    FinalEstable_1.0.0

---
+++ src/main/java/com/techtitans/service/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.techtitans.service;
+
+/**
+ *
+ * @author Donovan Morales
+ */
+public class NewClass {
+    
+}
